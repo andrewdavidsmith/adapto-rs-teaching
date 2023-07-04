25b9e58e23dbb8a5654a8687f6da319db3a58176
Andrew D Smith <andrewds@usc.edu>
2023-07-03 18:51:21 -0700

    Added several command line arguments for things that were hard-coded before. Still needs some verbosity.

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,14 @@
  */
 
 
+/// Program to cut adaptors from sequenced reads. Accepts one adaptor
+/// and will apply it to both ends in paired-end data. Removes Ns at
+/// the end of reads. Removes low quality bases at ends of reads.
+/// Output is compressed as bgzf. Input may be compressed as gz/bgzf
+/// or not. Extra threads help with compressing output and
+/// decompressing input.
+
+
 use clap::Parser;
 use std::process::ExitCode;
 
@@ -31,28 +39,44 @@ use std::process::ExitCode;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// fastq file
+    /// Fastq input file
     #[structopt(required=true)]
     fastq: String,
 
-    /// second fastq file for paired
+    /// Paired-end input second fastq file
     #[structopt(required=false)]
     pfastq: Option<String>,
 
-    /// output file
+    /// Output file
     #[arg(short, long)]
     out: String,
 
-    /// second output file for paired
+    /// Second output file for paired-end reads
     #[structopt(required=false)]
     #[arg(short, long)]
     pout: Option<String>,
 
-    /// quality score cutoff
+    /// Quality score cutoff
     #[arg(short, long, default_value_t = 20)]
     qual_cutoff: u8,
 
-    /// be verbose
+    /// Adaptor sequence
+    #[arg(short, long, default_value = "AGATCGGAAGAGC")]
+    adaptor: Option<String>,
+
+    /// Zip the output (does nothing...)
+    #[arg(short, long)]
+    zip: bool,
+
+    /// Threads to use
+    #[arg(short, long, default_value_t = 1)]
+    threads: u32,
+
+    /// Buffer size for reading input
+    #[arg(short, long, default_value_t = 256*1024)]
+    buffer_size: usize,
+
+    /// Be verbose
     #[arg(short, long)]
     verbose: bool,
 }
@@ -62,16 +86,31 @@ fn main() -> ExitCode {
 
     let args = Args::parse();
 
+    if args.threads <= 0 {
+        eprintln!("number of threads must be positive");
+        return ExitCode::FAILURE;
+    }
+
+    if args.buffer_size <= 0 {
+        eprintln!("buffer size must be positive");
+        return ExitCode::FAILURE;
+    }
+
+    let adaptor = args.adaptor.unwrap().into_bytes();
+
+    use radapt::process_reads;
     if let Some(pfastq) = args.pfastq {
         if let Some(pout) = args.pout {
-            radapt::process_reads(&pfastq, &pout, args.qual_cutoff).unwrap();
+            process_reads(args.zip, args.threads, args.buffer_size, &adaptor,
+                          &pfastq, &pout, args.qual_cutoff).unwrap();
         }
         else {
             eprintln!("specifying two inputs requires two outputs");
             return ExitCode::FAILURE;
         }
     }
-    radapt::process_reads(&args.fastq, &args.out, args.qual_cutoff).unwrap();
+    process_reads(args.zip, args.threads, args.buffer_size, &adaptor,
+                  &args.fastq, &args.out, args.qual_cutoff).unwrap();
 
     ExitCode::SUCCESS
 }
